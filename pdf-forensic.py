b1f5a717a7ddecc6fd8c7181fc7b49927dcdde0d
wqwe <57851869+ewinrahman@users.noreply.github.com>
2025-06-20 10:29:28 +0800

    Update pdf-forensic.py
    Add embedded URL extraction function def getUrl()

--- pdf-forensic.py
+++ pdf-forensic.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python
+import argparse
 import PyPDF2
-import optparse
-from PyPDF2 import PdfFileReader
-
-def banner():
-	print '##########################################'
-	print '##                                      ##'
-	print '##      Author: Sazzad Ovi              ##'
-	print '##                                      ##'
-	print '##           XOVIM001                   ##'
-	print '##                                      ##'
-	print '##                                      ##'
-	print '##########################################'
-
-banner()
 
 def printMeta(filename):
-	pdfFile = PdfFileReader(file(filename, 'rb'))
-	docInfo = pdfFile.getDocumentInfo()
-	print('[*] PDF MetaData for: ' + str(filename))
-	for metaItem in docInfo:
-		print('[+] ' + metaItem + ': ' + docInfo[metaItem])
+	with open(filename, 'rb') as f:
+		pdfFile = PyPDF2.PdfReader(f)
+		docInfo = pdfFile.metadata
+		print('[*] PDF MetaData for: ' + str(filename))
+		for metaItem in docInfo:
+		    print('[+] ' + metaItem + ': ' + docInfo[metaItem])
+
+def getUrl(filename):
+	with open(filename, 'rb') as f:
+		reader = PyPDF2.PdfReader(filename)
+		for page_num, page in enumerate(reader.pages):
+			if "/Annots" in page:
+				annotations = page["/Annots"]
+				for annot in annotations:
+					annot_obj = annot.get_object()
+					if "/A" in annot_obj:
+						action = annot_obj["/A"]
+						if "/URI" in action:
+							print(f"Page {page_num + 1}: {action['/URI']}")
+	
 
 def main():
-	parser = optparse.OptionParser('usage program: -F <PDF file name>')
-	parser.add_option('-F', dest='fileName', type='string',\
-		help='Please specify the PDF File name')
-	(options, args) = parser.parse_args()
-	fileName = options.fileName
-	if fileName == None:
-		print(parser.usage)
-		exit(0)
-	else:
-		printMeta(fileName)
+	parser = argparse.ArgumentParser('usage program: -F <PDF file name>')
+	parser.add_argument('-F', '--file', dest='fileName', required=True, help='Please specify the PDF File name')
+	args = parser.parse_args()
+	printMeta(args.fileName)
+	getUrl(args.fileName)
+
 if __name__ == '__main__':
 	main()
