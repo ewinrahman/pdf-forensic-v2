5c959068e02dd0eb4737e47fc387fd2e7ee91c7b
wqwe <57851869+ewinrahman@users.noreply.github.com>
2025-06-25 19:04:53 +0800

    Update pdf-forensic.py
    Added Plaintext URL extraction function

--- pdf-forensic.py
+++ pdf-forensic.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 import argparse
 import PyPDF2
+import re
 
 def printMeta(filename):
 	with open(filename, 'rb') as f:
 		pdfFile = PyPDF2.PdfReader(f)
 		docInfo = pdfFile.metadata
-		print('[*] PDF MetaData for: ' + str(filename))
+		print('\n[*] PDF MetaData for: ' + str(filename))
 		for metaItem in docInfo:
 		    print('[+] ' + metaItem + ': ' + docInfo[metaItem])
 
 def getUrl(filename):
 	with open(filename, 'rb') as f:
 		reader = PyPDF2.PdfReader(filename)
+		print('\nEmbedded URL(s):')
 		for page_num, page in enumerate(reader.pages):
 			if "/Annots" in page:
 				annotations = page["/Annots"]
@@ -22,7 +24,18 @@ def getUrl(filename):
 						action = annot_obj["/A"]
 						if "/URI" in action:
 							print(f"Page {page_num + 1}: {action['/URI']}")
-	
+
+def getPlainUrl(filename):
+	with open(filename, 'rb') as f:
+		read = PyPDF2.PdfReader(filename)
+		print('\nPlain text URL(s):')
+		for page_num, page in enumerate(read.pages):
+			if page:
+				text = str(page.extract_text())
+				plainurl = re.findall(r'http.*\w+', text)
+				if plainurl:
+					print(f'Page {page_num+1}: {plainurl}')
+
 
 def main():
 	parser = argparse.ArgumentParser('usage program: -F <PDF file name>')
@@ -30,6 +43,7 @@ def main():
 	args = parser.parse_args()
 	printMeta(args.fileName)
 	getUrl(args.fileName)
+	getPlainUrl(args.fileName)
 
 if __name__ == '__main__':
 	main()
